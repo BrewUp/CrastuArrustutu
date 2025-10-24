127ab13ebd5e52c2081f15b68bbfa01194652711
Alberto Acerbis <alberto.acerbis@gmail.com>
2025-10-24 11:40:52 +0200

    Add sample test

---
+++ src/Carnizzaro/CrastuArrustutu.Carnizzaro.Tests/Entities/ExampleTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Muflone.Messages.Commands;
+using Muflone.Messages.Events;
+using Muflone.SpecificationTests;
+
+namespace CrastuArrustutu.Carnizzaro.Tests.Entities;
+
+public class OrderCreatePurchaseOrderSuccessful : CommandSpecification<CreatePurchaseOrder>
+{
+    protected override IEnumerable<DomainEvent> Given()
+    {
+        yield break;
+    }
+
+    protected override CreatePurchaseOrder When()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override ICommandHandlerAsync<CreatePurchaseOrder> OnHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IEnumerable<DomainEvent> Expect()
+    {
+        throw new NotImplementedException();
+    }
+}
